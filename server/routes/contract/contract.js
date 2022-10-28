385935ee36085902df5ce8fc8593bd3610ed7929
yiminwook <grs0416@gmail.com>
2022-10-28 17:16:43 +0900

    add login, logout API

--- server/routes/contract/contract.js
+++ server/routes/contract/contract.js
@@ -5,14 +5,13 @@ require('dotenv').config();
 const contractAddr = process.env.ERC20_CONTRACT_ADDRESS
 const serverAddr = process.env.SERVER_ADDRESS;
 const transferTo = process.env.DUMMY_ADDRESS;
-/* exchange router listing. */
 
-router.put('/', function(req, res, next) {
-  res.send('here is exchange router');
+router.post('/eth', async (req, res, next) => {
+  res.send('here is contract/eth router');
 });
- 
-/* GET users router listing. */
-router.get('/', async (req, res, next) => {
+
+/* transfer router listing. */
+router.post('/token', async (req, res, next) => {
   // const abi = require('../../web3/ABI');
   const ERC20abi = require('../../web3/ERC20');
   const Contract = require('web3-eth-contract');
@@ -24,7 +23,12 @@ router.get('/', async (req, res, next) => {
   
   console.log(result)
   console.log(balance)
-  res.send('here is users/transfer router');
+  res.send('here is contract/token router');
 });
+
+router.post('/mint', async (req, res, next) => {
+  res.send('here is contract/mint router');
+});
+
 
 module.exports = router;
