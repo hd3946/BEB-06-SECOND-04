5e861a5565d152604770cbf57de3ca88e647516d
kimnahye <nahye7670@gmail.com>
2022-10-28 11:11:26 +0900

    add: router,sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Sidebar from "./component/common/sidebar/Sidebar";
+// import Main from './component/pages/sidebar/Main';
+// import Detail from './component/pages/sidebar/Detail';
+// import Mint from './component/pages/sidebar/Mint';
+// import Mypage from './component/pages/sidebar/Mypage';
+// import Signin from './component/pages/sidebar/Signin';
+// import Signup from './component/pages/sidebar/Signup';
 
 function App() {
   return (
     <div className="App">
-      <div>test</div>
+      <div>
+        <BrowserRouter>
+          <Sidebar />
+          <Routes>
+            {/* <Route path="/" element={<Main />}></Route>
+            <Route path="/detail" element={<Detail />}></Route>
+            <Route path="/mint" element={<Mint />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            <Route path="/signin" element={<Signin />}></Route>
+            <Route path="/signup" element={<Signup />}></Route> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ client/src/component/common/sidebar/Sidebar.js
@@ -0,0 +1,91 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faImage } from "@fortawesome/free-regular-svg-icons";
+const StyledAll = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
+
+const StyledLogo = styled.div`
+  width: 33px;
+  height: 33px;
+  border: 2px solid #03a9f4;
+  border-radius: 8px;
+  text-align: center;
+  line-height: 34px;
+  font-size: 14px;
+  display: inline-block;
+  margin: 27px 30px 13px 59px;
+  color: #03a9f4;
+`;
+
+const StyledSidebar = styled.div`
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 9rem;
+  background-color: #e0e0e0;
+`;
+
+const StyledButton = styled.a`
+  padding: 21px 23px 11px 49px;
+  font-size: 11px;
+  color: black;
+  display: block;
+  transition: 0.3s;
+`;
+
+const StyledButton2 = styled.div`
+  font-size: 6px;
+  text-align: center;
+  color: white;
+  padding: 6px 1px 1px 5px;
+  width: 58px;
+  height: 16px;
+  border-radius: 15px 15px 15px 15px;
+  margin: 13px 7px 3px 45px;
+  background-color: #5fc4fd;
+`;
+
+function Sidebar() {
+  return (
+    <StyledAll>
+      <StyledSidebar>
+        <StyledLogo>F4</StyledLogo>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <StyledButton>
+            <FontAwesomeIcon icon={faHouse} style={{ color: "white" }} /> &nbsp;
+            Home
+          </StyledButton>
+        </Link>
+        <Link to="/mint" style={{ textDecoration: "none" }}>
+          <StyledButton>
+            <FontAwesomeIcon icon={faImage} style={{ color: "white" }} />
+            &nbsp;&nbsp; Minting
+          </StyledButton>
+        </Link>
+
+        <Link to="/mypage" style={{ textDecoration: "none" }}>
+          <StyledButton>
+            <FontAwesomeIcon icon={faUser} style={{ color: "white" }} />
+            &nbsp;&nbsp;&nbsp;Mypage
+          </StyledButton>
+        </Link>
+        <StyledButton></StyledButton>
+        <StyledButton></StyledButton>
+
+        <Link to="/signin" style={{ textDecoration: "none" }}>
+          <StyledButton2>SignIn</StyledButton2>
+        </Link>
+        <Link to="/signup" style={{ textDecoration: "none" }}>
+          <StyledButton2>SignUp</StyledButton2>
+        </Link>
+      </StyledSidebar>
+    </StyledAll>
+  );
+}
+
+export default Sidebar;
