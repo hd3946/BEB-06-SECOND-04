f04d6ec5672a59e339d9fcdcf33d8e8fdc2147b3
MJKM <rhkrrbaudgg@naver.com>
2022-10-30 16:52:14 +0900

    add test route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Detail from "./component/pages/detail/DetailPage";
 import Mint from "./component/pages/minting/MintingPage";
 import Mypage from "./component/pages/mypage/MyPage";
 import SignPage from "./component/pages/sign/SignPage";
+import Loading from "./component/common/loading/Loading";
 
 function App() {
   return (
@@ -19,9 +20,12 @@ function App() {
               <Route path="/" element={<Main />}></Route>
               <Route path="/detail" element={<Detail />}></Route>
               <Route path="/mint" element={<Mint />}></Route>
-              <Route path="/mypage2" element={<Mypage />}></Route>
+              <Route path="/mypage" element={<Mypage />}></Route>
               {/* sign은 Sidebar 컴포넌트 밑으로 빼고 redux로 on/off 관리 */}
               <Route path="/sign" element={<SignPage />}></Route>
+
+              {/* 테스트 */}
+              <Route path="/test" element={<Loading />}></Route>
             </Routes>
           </div>
         </BrowserRouter>
