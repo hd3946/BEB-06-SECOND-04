3bd645d93fd9344fe0d0a4d8402808f237773ed2
yiminwook <grs0416@gmail.com>
2022-10-31 14:04:03 +0900

    update post API user table address 연동

--- server/routes/post/post.js
+++ server/routes/post/post.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 const tokenAddr = process.env.TOKEN_CONTRACT_ADDRESS
 const serverAddr = process.env.SERVER_ADDRESS;        //가나슈1
-const userAddr = process.env.USER_ADDRESS;          //가나슈2
 
 const Contract = require('web3-eth-contract');
 Contract.setProvider('http://localhost:7545');
@@ -15,18 +14,17 @@ var { upload } = require('./upload')
 // const { isLoggedIn } = require('./middlewares'); 
 
 /* post router listing. */
-//사진 한장~! , 
 router.post('/', upload.single('post'), async (req, res, next) => {
   if(!req.cookies.loginData) return res.status(401).json('로그인되어 있지 않습니다.')
   try {
-    const userId = req.cookies.loginData.id;
+    const { id, address }  = req.cookies.loginData;
     const { title, content } = req.body;
-    console.log('유저 포스트 업로드', userId);  
+    console.log('유저 포스트 업로드', id);  
     const post = await Post.create({
       title: title,
       content: content,
       // img: req.file.location, 
-      userId: userId,
+      userId: id,
     });
     //해쉬태그 
     // const hashtags = req.body.content.match(/#[^\s#]*/g);
@@ -41,19 +39,19 @@ router.post('/', upload.single('post'), async (req, res, next) => {
     //   await post.addHashtags(result.map(r => r[0]));
     // }
     if (post) { //게시글 하나 작성할때마다 토큰을 하나 전송
-      const transferToken = await tokenContract.methods.transfer(userAddr, 1).send({ from: serverAddr }) //user에게 token를 전송
-      const tokenBalance = await tokenContract.methods.balanceOf(userAddr).call()
+      const transferToken = await tokenContract.methods.transfer(address, 1).send({ from: serverAddr }) //user에게 token를 전송
+      const tokenBalance = await tokenContract.methods.balanceOf(address).call()
       return res.status(200).json({
         status: true,
-        tokenBalance: tokenBalance,
         message: 'Post Success',
+        tokenBalance: tokenBalance,
       }); 
     } else {
-      const tokenBalance = await tokenContract.methods.balanceOf(userAddr).call()
+      const tokenBalance = await tokenContract.methods.balanceOf(address).call()
       return res.status(200).json({
         status: false,
-        tokenBalance: tokenBalance,
         message: 'Post fail',
+        tokenBalance: tokenBalance,
       });
     }
   } catch (err) {
