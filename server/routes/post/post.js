15b049e083605264ebfc915c27638b994fc74706
yiminwook <grs0416@gmail.com>
2022-10-31 13:36:49 +0900

    update post API

--- server/routes/post/post.js
+++ server/routes/post/post.js
@@ -1,34 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const tokenAddr = process.env.TOKEN_CONTRACT_ADDRESS
+const serverAddr = process.env.SERVER_ADDRESS;        //가나슈1
+const userAddr = process.env.USER_ADDRESS;          //가나슈2
+
+const Contract = require('web3-eth-contract');
+Contract.setProvider('http://localhost:7545');
+const tokenABI = require('../../web3/tokenABI'); 
+const tokenContract = new Contract(tokenABI, tokenAddr);
+
+var { User, Post } = require('../../models/index');
 var { upload } = require('./upload')  
 // const { isLoggedIn } = require('./middlewares'); 
 
 /* post router listing. */
-router.post('/', function(req, res, next) {
-  res.send('here is post router');
-});
-
-router.post('/', function(req, res, next) {
-  res.send('here is post router');
-});
-
-router.post('/', function(req, res, next) {
-  res.send('here is post router');
-});
-
-router.post('/', function(req, res, next) {
-  res.send('here is post router');
-});
-
 //사진 한장~! , 
 router.post('/', upload.single('post'), async (req, res, next) => {
+  if(!req.cookies.loginData) return res.status(401).json('로그인되어 있지 않습니다.')
   try {
-    console.log('유저 포스트 업로드', req.user.id);  //req.body.content
+    const userId = req.cookies.loginData.id;
+    const { title, content } = req.body;
+    console.log('유저 포스트 업로드', userId);  
     const post = await Post.create({
-      content: 'test',
-      img: req.file.location,
-      UserId: req.user.id,
+      title: title,
+      content: content,
+      // img: req.file.location, 
+      userId: userId,
     });
     //해쉬태그 
     // const hashtags = req.body.content.match(/#[^\s#]*/g);
@@ -42,20 +40,25 @@ router.post('/', upload.single('post'), async (req, res, next) => {
     //   );
     //   await post.addHashtags(result.map(r => r[0]));
     // }
-    if(post){
+    if (post) { //게시글 하나 작성할때마다 토큰을 하나 전송
+      const transferToken = await tokenContract.methods.transfer(userAddr, 1).send({ from: serverAddr }) //user에게 token를 전송
+      const tokenBalance = await tokenContract.methods.balanceOf(userAddr).call()
       return res.status(200).json({
         status: true,
+        tokenBalance: tokenBalance,
         message: 'Post Success',
       }); 
-    }else{
+    } else {
+      const tokenBalance = await tokenContract.methods.balanceOf(userAddr).call()
       return res.status(200).json({
         status: false,
+        tokenBalance: tokenBalance,
         message: 'Post fail',
       });
     }
-  } catch (error) {
-    console.error(error);
-    next(error);
+  } catch (err) {
+    console.error(err);
+    next(err);
   }
 });
 
