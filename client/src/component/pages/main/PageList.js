1870e3482e46bf4e5c5687e373975c67b9f60dc1
MJKM <rhkrrbaudgg@naver.com>
2022-11-02 22:55:57 +0900

    update tap css..

--- client/src/component/pages/main/PageList.js
+++ client/src/component/pages/main/PageList.js
@@ -4,6 +4,7 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import Page from "../../common/page/Page";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const PageListBox = styled.div`
   display: flex;
@@ -17,19 +18,29 @@ const PageListBox = styled.div`
       padding-bottom: 5px;
       text-align: center;
       font-weight: 600;
-      border-bottom: 3px solid rgb(82, 192, 255);
+      border-bottom: ${(props) =>
+        props.tap === "ALL" ? "3px solid rgb(82, 192, 255)" : "0px"};
       cursor: pointer;
+    }
+    .tapSearch {
+      border-bottom: ${(props) =>
+        props.tap === "SEARCH" ? "3px solid rgb(82, 192, 255)" : "0px"};
     }
   }
 `;
 
 const PageList = () => {
   const [tap, setTap] = useState("ALL");
-
   const { list, filterList } = useSelector((state) => state.post);
 
+  useEffect(() => {
+    if (filterList.length > 0) {
+      setTap("SEARCH");
+    }
+  }, [filterList]);
+
   return (
-    <PageListBox>
+    <PageListBox tap={tap} filterColor={filterList.length}>
       <div className="pageHeader">
         <div
           className="tapAll"
@@ -40,26 +51,22 @@ const PageList = () => {
           ALL
         </div>
 
-        <div className="tapAll" onClick={() => setTap("SEARCH")}>
+        <div className="tapAll tapSearch" onClick={() => setTap("SEARCH")}>
           SEARCH
         </div>
       </div>
       {tap === "ALL" ? (
         list.length > 0 ? (
-          list.map((data, index) => (
-            // data 에서 페이지 id를 받아 link로 보냄
-
-            <Page pos="main" key={index} data={data} />
-          ))
+          list.map((data, index) => <Page key={index} data={data} />)
         ) : (
           <div>작성된 글이 없어요!</div>
         )
       ) : filterList.length > 0 ? (
-        filterList.map((data, index) => (
-          <Page pos="main" key={index} data={data} />
-        ))
+        filterList.map((data, index) => <Page key={index} data={data} />)
       ) : (
-        <div>검색된 글이 없어요!</div>
+        <div className="cc" style={{ height: "150px" }}>
+          검색된 글이 없어요!
+        </div>
       )}
     </PageListBox>
   );
