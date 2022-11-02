fecb715deb842fa22a84be45c56d7539506fc913
MJKM <rhkrrbaudgg@naver.com>
2022-11-02 22:48:11 +0900

    no Change

--- server/routes/post/post.js
+++ server/routes/post/post.js
@@ -1,29 +1,37 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import upload from './upload.js';
+import upload from "./upload.js";
 
-import { db } from '../../models/index.js';
-const { User, Post, Comment } = db; 
-import ganache  from "../../web3/web3.js";
+import { db } from "../../models/index.js";
+const { User, Post, Comment } = db;
+import ganache from "../../web3/web3.js";
 const { getTokenBalance, giveContribution } = ganache;
 
 /* post router listing. */
 router.get("/list", upload.single("post"), async (req, res, next) => {
   try {
     const postList = await Post.findAll({
-      attributes: [['id', 'postId'], 'title', 'content', 'createdAt', 'updatedAt' ],
-      include: 
-      [
-        { model: User, 
-          attributes: ['email', 'nickname'] 
+      attributes: [
+        ["id", "postId"],
+        "title",
+        "content",
+        "createdAt",
+        "updatedAt",
+      ],
+      include: [
+        { model: User, attributes: ["email", "nickname"] },
+        {
+          model: Comment,
+          attributes: [
+            ["id", "commentId"],
+            "content",
+            "createdAt",
+            "updatedAt",
+            "commenter",
+            "postId",
+          ],
+          include: { model: User, attributes: ["email", "nickname"] },
         },
-        { model: Comment,
-          attributes: [['id', 'commentId'], 'content', 'createdAt', 'updatedAt', 'commenter', 'postId'], 
-          include: 
-          { model: User, 
-            attributes: ['email', 'nickname'] 
-          }
-        }
       ],
     });
     return res.status(200).json({
@@ -91,16 +99,18 @@ router.post("/edit", upload.single("post"), async (req, res, next) => {
     const { id } = req.cookies.loginData;
     const { postId, title, content } = req.body;
     const data = await Post.findOne({ where: { id: postId } });
-    const postingUser = data.toJSON().userId
-    if (postingUser === id) { //수정은 작성자만 가능
-      const update = data.update({ //update 날짜는 자동으로 변경
-          title: title,
-          content: content,
-      })
+    const postingUser = data.toJSON().userId;
+    if (postingUser === id) {
+      //수정은 작성자만 가능
+      const update = data.update({
+        //update 날짜는 자동으로 변경
+        title: title,
+        content: content,
+      });
       return res.status(200).json({
         status: true,
         message: "edit success",
-      })
+      });
     } else {
       return res.status(401).json({
         status: false,
@@ -120,7 +130,8 @@ router.post("/delete", upload.single("post"), async (req, res, next) => {
     const { postId } = req.body;
     const data = await Post.findOne({ where: { id: postId } });
     const postingUser = data.toJSON().userId;
-    if (postingUser === id) { //삭제는 작성자만 가능
+    if (postingUser === id) {
+      //삭제는 작성자만 가능
       data.destroy();
       return res.status(200).json({
         status: true,
