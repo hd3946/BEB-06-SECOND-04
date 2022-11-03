2567c1ce761ce51da02d1020eb1c4b6f7e9c92b7
MJKM <rhkrrbaudgg@naver.com>
2022-11-03 17:40:40 +0900

    fix code

--- client/src/api/sign.js
+++ client/src/api/sign.js
@@ -1,22 +1,17 @@
-// 로그인, 회원 가입, (ex) 회원 탈퇴
 import { sign } from "./index";
 
-// 회원가입 API
 function registerInfo() {
   return sign.get("info");
 }
 
-// 회원가입 API
 function registerUser(userData) {
   return sign.post("signup", userData);
 }
 
-// 로그인 API
 function loginUser(userData) {
   return sign.post("signin", userData);
 }
 
-// 로그아웃 API
 function logoutUser() {
   return sign.post("signout");
 }
